6acc8366157226447eea6717f4b99ded5a11f398
goblin-high-priest <liangqi.chen88@gmail.com>
2025-05-12 01:38:35 +1000

    2302 updated

--- python/2302_Count_Subarrays_With_Score_Less_Than_K.py
+++ python/2302_Count_Subarrays_With_Score_Less_Than_K.py
@@ -2,13 +2,13 @@ class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         left = 0
         res = 0
-        tmp = 0
+        cur_sum = 0
 
         for right, x in enumerate(nums):
-            tmp += x
+            cur_sum += x
 
-            while tmp * (right - left + 1) >= k:
-                tmp -= nums[left]
+            while cur_sum * (right - left + 1) >= k:
+                cur_sum -= nums[left]
                 left += 1
                 
             res += right - left + 1

0babdcca108b9e658c645ecf10ea9713b8522fef
goblin-high-priest <liangqi.chen88@gmail.com>
2025-06-18 03:06:12 +1000

    2187 finished

---
+++ python/2187_Minimum_Time_to_Complete_Trips.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        left = min(time)
+        right = min(time) * totalTrips
+
+        while left < right:
+            mid = (left + right) // 2
+            trips = sum([mid // t for t in time])
+
+            if trips < totalTrips:
+                left = mid + 1
+            else:
+                right = mid 
+            
+        return left
+

14fe3f3a2d6070bde18a9584de59fdf91f975fa7
goblin-high-priest <liangqi.chen88@gmail.com>
2025-07-10 00:43:02 +1000

    0178 finished

---
+++ postgresql/0178_Rank_Scores.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    score,
+    rank
+FROM (
+    SELECT
+        score,
+        DENSE_RANK() OVER (ORDER BY score DESC) AS rank
+    FROM
+        Scores
+)

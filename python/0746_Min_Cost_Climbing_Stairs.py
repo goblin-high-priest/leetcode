7f11b3a4d9f632c76e261f17c0fa5328f46d4084
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-03 18:24:33 +1000

    0746 finished

---
+++ python/0746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+        @cache
+        def dfs(i):
+
+            if i <= 1:
+                return 0
+            
+            return min(cost[i-1] + dfs(i - 1), cost[i-2] + dfs(i - 2))
+
+        return dfs(n)

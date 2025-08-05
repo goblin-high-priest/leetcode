de994d181ae3421daab6712ffb52f3dd82f2416e
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-05 23:25:04 +1000

    0064 finished

---
+++ python/0064_Minimum_Path_Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        @cache
+        def dfs(i, j):
+
+            if i == 0 and j == 0:
+                return grid[i][j]
+                
+            if i < 0 or j < 0:
+                return float('inf')
+
+            return grid[i][j] + min(dfs(i - 1, j), dfs(i, j - 1))
+        
+        return dfs(len(grid) - 1, len(grid[0]) - 1)

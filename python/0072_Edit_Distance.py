f9aef20d6c235f76f33207efbe883e4129ea5d8e
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-13 06:08:27 +1000

    0072 finished

---
+++ python/0072_Edit_Distance.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+
+        @cache
+        def dfs(i, j):
+
+            if i < 0:
+                return j + 1
+            
+            if j < 0:
+                return i + 1
+            
+            if word1[i] == word2[j]:
+                return dfs(i - 1, j - 1)
+            
+            return min(dfs(i - 1, j), dfs(i, j - 1), dfs(i - 1, j - 1)) + 1
+        
+        return dfs(n - 1, m - 1)

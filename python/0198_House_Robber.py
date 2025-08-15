82ec5c2b00eacd4805bfa102bcfca8f400eefff1
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-16 04:09:15 +1000

    0198 modified

--- python/0198_House_Robber.py
+++ python/0198_House_Robber.py
@@ -1,13 +1,31 @@
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         n = len(nums)
+        
+#         @cache
+#         def dfs(i):
+
+#             if i < 0:
+#                 return 0
+            
+#             return max(dfs(i - 1), dfs(i - 2) + nums[i])
+        
+#         return dfs(n - 1)
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         f = [0] * (len(nums) + 2)
+
+#         for i, x in enumerate(nums):
+#             f[i+2] = max(f[i+1], f[i] + x)
+        
+#         return f[-1]
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        @cache
-        def dfs(i):
+        f0 = f1 = 0
 
-            if i < 0:
-                return 0
-            
-            return max(dfs(i - 1), dfs(i - 2) + nums[i])
+        for i, x in enumerate(nums):
+            f0, f1 = f1, max(f0 + x, f1)
         
-        return dfs(n - 1)
+        return f1

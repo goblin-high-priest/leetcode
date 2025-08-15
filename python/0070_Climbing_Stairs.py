09fd41366e4a16851c35e1ef3ff5a2a4eb4fb3cc
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-16 04:24:56 +1000

    0070 modified

--- python/0070_Climbing_Stairs.py
+++ python/0070_Climbing_Stairs.py
@@ -1,18 +1,37 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         cache = [-1] * (n + 1)
+
+#         def dfs(i):
+
+#             if i == 0 or i == 1:
+#                 return 1
+            
+#             if cache[i] != -1:
+#                 return cache[i]
+
+#             res = dfs(i - 1) + dfs(i - 2)
+#             cache[i] = res
+
+#             return res
+
+#         return dfs(n)
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         f = [0] * (n + 1)
+#         f[0] = f[1] = 1
+
+#         for i in range(2, n + 1):
+#             f[i] = f[i-1] + f[i-2]
+        
+#         return f[-1]
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = [-1] * (n + 1)
+        f0 = f1 = 1
 
-        def dfs(i):
-
-            if i == 0 or i == 1:
-                return 1
-            
-            if cache[i] != -1:
-                return cache[i]
-
-            res = dfs(i - 1) + dfs(i - 2)
-            cache[i] = res
-
-            return res
-
-        return dfs(n)
+        for i in range(2, n + 1):
+            f0, f1 = f1, f0 + f1
+        
+        return f1

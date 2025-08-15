f7f10639509463fdf81f784d457c012177c5ba21
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-16 04:50:35 +1000

    0746 modified

--- python/0746_Min_Cost_Climbing_Stairs.py
+++ python/0746_Min_Cost_Climbing_Stairs.py
@@ -1,13 +1,31 @@
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         n = len(cost)
+
+#         @cache
+#         def dfs(i):
+
+#             if i <= 1:
+#                 return 0
+            
+#             return min(cost[i-1] + dfs(i - 1), cost[i-2] + dfs(i - 2))
+
+#         return dfs(n)
+
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         f = [0] * (len(cost) + 1)
+        
+#         for i in range(2, len(cost) + 1):
+#             f[i] = min(f[i-1] + cost[i-1], f[i-2] + cost[i-2])
+        
+#         return f[-1]
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
+        f0 = f1 = 0
 
-        @cache
-        def dfs(i):
-
-            if i <= 1:
-                return 0
-            
-            return min(cost[i-1] + dfs(i - 1), cost[i-2] + dfs(i - 2))
-
-        return dfs(n)
+        for i in range(2, len(cost) + 1):
+            f0, f1 = f1, min(f0 + cost[i - 2], f1 + cost[i-1])
+        
+        return f1

7b59e99a8f225ea5c85e27afbb80738193e0407a
goblin-high-priest <liangqi.chen88@gmail.com>
2025-08-17 08:21:50 +0800

    2466 modified

--- python/2466_Count_Ways_To_Build_Good_Strings.py
+++ python/2466_Count_Ways_To_Build_Good_Strings.py
@@ -1,15 +1,30 @@
+# class Solution:
+#     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+#         MOD = 1_000_000_007
+        
+#         @cache
+#         def dfs(i):
+            
+#             if i == 0:
+#                 return 1
+#             elif i < 0:
+#                 return 0
+#             else:
+#                 return (dfs(i - zero) + dfs(i - one)) % MOD
+            
+#         return (sum([dfs(i) for i in range(low, high + 1)])) % MOD
+
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         MOD = 1_000_000_007
-        
-        @cache
-        def dfs(i):
+        f = [1] + [0] * high
+
+        for i in range(1, high + 1):
+
+            if i >= zero:
+                f[i] = f[i-zero]
             
-            if i == 0:
-                return 1
-            elif i < 0:
-                return 0
-            else:
-                return (dfs(i - zero) + dfs(i - one)) % MOD
+            if i >= one:
+                f[i] = (f[i] + f[i-one]) % MOD
             
-        return (sum([dfs(i) for i in range(low, high + 1)])) % MOD
+        return sum(f[low:]) % MOD

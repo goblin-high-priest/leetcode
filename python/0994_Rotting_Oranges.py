6e8ac11ece841cb7d6e9ee8e3fd6a83aa94eda98
goblin-high-priest <liangqi.chen88@gmail.com>
2025-10-11 16:51:29 -0400

    0994 finished

---
+++ python/0994_Rotting_Oranges.py
@@ -0,0 +1,34 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh = 0
+        time = 0
+
+        for i in range(ROWS):
+            for j in range(COLS):
+
+                if grid[i][j] == 1:
+                    fresh += 1
+                
+                if grid[i][j] == 2:
+                    q.append((i, j))
+        
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        while q and fresh > 0:
+
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+
+                    if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == 1:
+                        q.append((row, col))
+                        fresh -= 1
+                        grid[row][col] = 2
+            
+            time += 1
+        
+        return time if fresh == 0 else -1

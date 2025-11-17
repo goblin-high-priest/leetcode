68418690b498856461dc98db75b63b76e4d961a7
goblin-high-priest <liangqi.chen88@gmail.com>
2025-11-17 11:39:22 -0500

    0032 finished

---
+++ python/0032_Longest_Valid_Parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        """
+        DP
+        
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        n = len(s)
+        dp = [0] * n
+        res = 0
+
+        for i in range(1, n):
+
+            if s[i] == ')':
+
+                if s[i-1] == '(':
+                    dp[i] = dp[i-2] + 2 if i >= 2 else 2
+                elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
+                    dp[i] = dp[i-1] + dp[i - dp[i-1] - 2] + 2 if i - dp[i-1] - 2 > 0 else dp[i-1] + 2
+                
+                res = max(res, dp[i])
+        
+        return res

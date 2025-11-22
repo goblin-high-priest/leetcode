e5d93dd27a127321338644c114530c38ae93476e
goblin-high-priest <liangqi.chen88@gmail.com>
2025-11-22 17:37:47 -0500

    1019 finished

---
+++ python/1019_Next_Greater_Node_In_Linked_List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        res = []
+        stack = []
+
+        while head:
+            
+            while stack and head.val > res[stack[-1]]:
+                res[stack.pop()] = head.val
+            
+            stack.append(len(res))
+            res.append(head.val)
+            head = head.next
+        
+        for i in stack:
+            res[i] = 0
+        
+        return res

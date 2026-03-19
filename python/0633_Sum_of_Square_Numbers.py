327adb19aa32b8e3ac018334307dd1dd1f656f0d
goblin-high-priest <liangqi.chen88@gmail.com>
2026-03-19 17:50:41 -0400

    0633 finished

---
+++ python/0633_Sum_of_Square_Numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a, b = 0, isqrt(c)
+
+        while a <= b:
+            s = a ** 2 + b ** 2
+
+            if s == c:
+                return True
+            elif s < c:
+                a += 1
+            else:
+                b -= 1
+        
+        return False

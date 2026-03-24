c1822b525d09a2aefe8c159e365f21c65ba8a853
goblin-high-priest <liangqi.chen88@gmail.com>
2026-03-24 02:14:01 -0400

    1616 finished

---
+++ python/1616_Split_Two_Strings_to_Make_Palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        
+        def check(a: str, b: str) -> bool:
+            i, j = 0, len(a) - 1
+
+            while i < j and a[i] == b[j]:
+                i += 1
+                j -= 1
+            
+            s, t = a[i:j+1], b[i:j+1]
+
+            return s == s[::-1] or t == t[::-1]
+        
+        return check(a, b) or check(b, a)
+    

c9c3c4e279167894adef22d5e32bdf9e427c4bd1
goblin-high-priest <liangqi.chen88@gmail.com>
2026-04-13 18:10:21 -0400

    0001 finished

--- java/0001_Two_Sum.java
+++ java/0001_Two_Sum.java
@@ -1,28 +1,14 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashmap = new HashMap<>();
 
-        int[] res = new int[2];
-        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
 
-        System.arraycopy(nums, 0, temp, 0, nums.length);
-        Arrays.sort(nums);
-        for (int i = 0, j = nums.length - 1; i < j;) {
-            if (nums[i] + nums[j] < target) {
-                i++;
-            } else if (nums[i] + nums[j] > target) {
-                j--;
-            } else {
-                final int num1 = nums[i];
-                final int num2 = nums[j];
-                int index1 = IntStream.range(0, temp.length).filter(x -> temp[x] == num1).findFirst().orElse(-1);
-                int index2 = IntStream.range(0, temp.length).filter(x -> temp[x] == num2 && x != index1).findFirst().orElse(-1);
-                res[0] = index1;
-                res[1] = index2;
-                return res;
-            }
+            if (hashmap.containsKey(nums[i])) return new int[]{hashmap.get(nums[i]), i};
+            hashmap.put(target - nums[i], i);
         }
+
         return null;
     }
-    
 }

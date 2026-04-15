0d871223fcf792fbee33d657f830b579059ef42f
goblin-high-priest <liangqi.chen88@gmail.com>
2026-04-15 17:53:22 -0400

    0141 finished

--- java/0141_Linked_List_Cycle.java
+++ java/0141_Linked_List_Cycle.java
@@ -9,19 +9,36 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         Set<ListNode> nodes = new HashSet<>();
+
+//         while(head != null){
+
+//             if(nodes.contains(head)){
+//                 return true;
+//             }
+
+//             nodes.add(head);
+//             head = head.next;
+
+//         }
+
+//         return false;
+        
+//     }
+// }
+
 class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> nodes = new HashSet<>();
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(head != null){
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
 
-            if(nodes.contains(head)){
-                return true;
-            }
-
-            nodes.add(head);
-            head = head.next;
-
+            if (slow == fast) return true;
         }
 
         return false;
